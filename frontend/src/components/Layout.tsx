60c3cff7a7df084e57340d262fe5bfb0583e780f
Ramzi Shodmonbekov <shodmonbekov516@gmail.com>
2024-05-16 00:19:01 +0600

    update Layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import Navbar from "./Navbar";
 import { useAppSelector } from "../redux/hooks";
 import Alert from "./Alert";
+import BackgroundImage from "../assets/images/house1.jpg";
 
 interface Props {
   children: ReactNode;
@@ -12,7 +13,15 @@ const Layout = (props: Props) => {
   return (
     <>
       <Navbar />
-      <div className="mt-[76px] md:mt-[84px]">
+      <div
+        className="mt-[76px] md:mt-[84px]"
+        style={{
+          backgroundImage: `url(${BackgroundImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+        }}
+      >
         {message && alertType && (
           <Alert message={message} alertType={alertType} />
         )}
